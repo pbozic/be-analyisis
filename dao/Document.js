db2d1488be7e2d915e9d53d9c7c8f0e9ea965203
Tadej Hiti <hiti.tadej@gmail.com>
2024-06-04 18:00:28 +0200

    extended documents DAO + implemented documents controller + routes api/documents

--- dao/Document.js
+++ dao/Document.js
@@ -1,5 +1,32 @@
 const prisma = require("../prisma/prisma");
 
+const getDocuments = async () => {
+	try {
+		return await prisma.documents.findMany({
+			include: {
+				files: true,
+			},
+		});
+	} catch (error) {
+		console.error("Error getting all documents:", error);
+		return new Error(error);
+	}
+};
+
+const getDocumentById = async (documentId) => {
+	try {
+		return await prisma.documents.findUnique({
+			where: { document_id: documentId },
+			include: {
+				files: true,
+			},
+		});
+	} catch (error) {
+		console.error("Error getting document by ID:", error);
+		return new Error(error);
+	}
+};
+
 const getDocumentsForBusiness = async (businessId) => {
 	try {
 		return await prisma.documents.findMany({
@@ -42,6 +69,20 @@ const getDocumentsForDriver = async (driverId) => {
 	}
 };
 
+const getDocumentsForUser = async (userId) => {
+	try {
+		return await prisma.documents.findMany({
+			where: { user_id: userId },
+			include: {
+				files: true,
+			},
+		});
+	} catch (error) {
+		console.error("Error getting documents for user:", error);
+		return new Error(error);
+	}
+};
+
 const getDocumentsForVehicle = async (vehicleId) => {
 	try {
 		return await prisma.documents.findMany({
@@ -56,110 +97,6 @@ const getDocumentsForVehicle = async (vehicleId) => {
 	}
 };
 
-const createDocument = async (documentData, filesData) => {
-	try {
-		return await prisma.documents.create({
-			data: {
-				...documentData,
-				files: {
-					create: filesData,
-				},
-			},
-			include: {
-				files: true,
-			},
-		});
-	} catch (error) {
-		console.error("Error creating document:", error);
-		return new Error(error);
-	}
-};
-
-const getDocumentById = async (documentId) => {
-	try {
-		return await prisma.documents.findUnique({
-			where: { document_id: documentId },
-			include: {
-				files: true,
-			},
-		});
-	} catch (error) {
-		console.error("Error getting document by ID:", error);
-		return new Error(error);
-	}
-};
-
-const deleteDocument = async (documentId) => {
-	try {
-		return await prisma.documents.delete({
-			where: { document_id: documentId },
-		});
-	} catch (error) {
-		console.error("Error deleting document:", error);
-		return new Error(error);
-	}
-};
-
-const updateDocumentExpirationDate = async (documentId, expirationDate) => {
-	try {
-		return await prisma.documents.update({
-			where: { document_id: documentId },
-			data: {
-				expiration_date: expirationDate,
-			},
-		});
-	} catch (error) {
-		console.error("Error updating document's expiration date:", error);
-		return new Error(error);
-	}
-};
-
-const updateDocumentIssueDate = async (documentId, issueDate) => {
-	try {
-		return await prisma.documents.update({
-			where: { document_id: documentId },
-			data: {
-				issue_date: issueDate,
-			},
-		});
-	} catch (error) {
-		console.error("Error updating document's issue date:", error);
-		return new Error(error);
-	}
-};
-
-const updateDocumentFiles = async (documentId, filesData) => {
-	try {
-		return await prisma.documents.update({
-			where: { document_id: documentId },
-			data: {
-				files: {
-					deleteMany: [{ document_id: documentId }], // Deletes all files associated with the document
-					create: filesData, // Creates new file entries
-				},
-			},
-			include: {
-				files: true,
-			},
-		});
-	} catch (error) {
-		console.error("Error updating document's files:", error);
-		return new Error(error);
-	}
-};
-
-const updateDocumentAdditionalInfo = async (documentId, jsonData) => {
-	try {
-		return await prisma.documents.update({
-			where: { document_id: documentId },
-			data: { additional_info: jsonData },
-		});
-	} catch (error) {
-		console.error("Error updating document's additional info:", error);
-		return new Error(error);
-	}
-};
-
 const getDocumentsByType = async (documentType) => {
 	try {
 		return await prisma.documents.findMany({
@@ -170,20 +107,6 @@ const getDocumentsByType = async (documentType) => {
 		});
 	} catch (error) {
 		console.error("Error getting documents by type:", error);
-		return new Error(error);
-	}
-};
-
-const getDocumentsForUser = async (userId) => {
-	try {
-		return await prisma.documents.findMany({
-			where: { user_id: userId },
-			include: {
-				files: true,
-			},
-		});
-	} catch (error) {
-		console.error("Error getting documents for user:", error);
 		return new Error(error);
 	}
 };
@@ -273,14 +196,160 @@ const getDocumentsForVehicleByType = async (vehicleId, documentType) => {
 	}
 };
 
+const createDocument = async (documentData, filesData) => {
+	try {
+		return await prisma.documents.create({
+			data: {
+				...documentData,
+				files: {
+					create: filesData,
+				},
+			},
+			include: {
+				files: true,
+			},
+		});
+	} catch (error) {
+		console.error("Error creating document:", error);
+		return new Error(error);
+	}
+};
+
+const updateDocumentExpirationDate = async (documentId, expirationDate) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: {
+				expiration_date: expirationDate,
+			},
+		});
+	} catch (error) {
+		console.error("Error updating document's expiration date:", error);
+		return new Error(error);
+	}
+};
+
+const updateDocumentIssueDate = async (documentId, issueDate) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: {
+				issue_date: issueDate,
+			},
+		});
+	} catch (error) {
+		console.error("Error updating document's issue date:", error);
+		return new Error(error);
+	}
+};
+
+const updateDocumentFiles = async (documentId, filesData) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: {
+				files: {
+					deleteMany: [{ document_id: documentId }], // Deletes all files associated with the document
+					create: filesData, // Creates new file entries
+				},
+			},
+			include: {
+				files: true,
+			},
+		});
+	} catch (error) {
+		console.error("Error updating document's files:", error);
+		return new Error(error);
+	}
+};
+
+const updateDocumentAdditionalInfo = async (documentId, jsonData) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { additional_info: jsonData },
+		});
+	} catch (error) {
+		console.error("Error updating document's additional info:", error);
+		return new Error(error);
+	}
+};
+
+const linkDocumentToUser = async (documentId, userId) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { user_id: userId },
+		});
+	} catch (error) {
+		console.error("Error linking document to user:", error);
+		return new Error(error);
+	}
+};
+
+const linkDocumentToVehicle = async (documentId, vehicleId) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { vehicle_id: vehicleId },
+		});
+	} catch (error) {
+		console.error("Error linking document to vehicle:", error);
+		return new Error(error);
+	}
+};
+
+const linkDocumentToDriver = async (documentId, driverId) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { driver_id: driverId },
+		});
+	} catch (error) {
+		console.error("Error linking document to driver:", error);
+		return new Error(error);
+	}
+};
+
+const linkDocumentToBusiness = async (documentId, businessId) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { business_id: businessId },
+		});
+	} catch (error) {
+		console.error("Error linking document to business:", error);
+		return new Error(error);
+	}
+};
+
+const linkDocumentToDeliveryPerson = async (documentId, deliveryPersonId) => {
+	try {
+		return await prisma.documents.update({
+			where: { document_id: documentId },
+			data: { delivery_person_id: deliveryPersonId },
+		});
+	} catch (error) {
+		console.error("Error linking document to delivery person:", error);
+		return new Error(error);
+	}
+};
+
+const deleteDocument = async (documentId) => {
+	try {
+		return await prisma.documents.delete({
+			where: { document_id: documentId },
+		});
+	} catch (error) {
+		console.error("Error deleting document:", error);
+		return new Error(error);
+	}
+};
+
 module.exports = {
 	createDocument,
+	getDocuments,
 	getDocumentById,
-	deleteDocument,
-	updateDocumentFiles,
-	updateDocumentExpirationDate,
-	updateDocumentIssueDate,
-	updateDocumentAdditionalInfo,
 	getDocumentsForUser,
 	getDocumentsForBusiness,
 	getDocumentsForDeliveryPerson,
@@ -292,4 +361,14 @@ module.exports = {
 	getDocumentsForDriverByType,
 	getDocumentsForDeliveryPersonByType,
 	getDocumentsForVehicleByType,
+	updateDocumentFiles,
+	updateDocumentExpirationDate,
+	updateDocumentIssueDate,
+	updateDocumentAdditionalInfo,
+	linkDocumentToUser,
+	linkDocumentToVehicle,
+	linkDocumentToDriver,
+	linkDocumentToBusiness,
+	linkDocumentToDeliveryPerson,
+	deleteDocument,
 };
---
+++ routes/api/documents.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const router = express.Router();
+
+const DocumentsController = require("../../controllers/DocumentsController");
+
+router.get('/', DocumentsController.listDocuments);
+router.get('/:documentId', DocumentsController.getDocumentById);
+router.get('/users/:userId', DocumentsController.getDocumentsForUser);
+router.get('/businesses/:businessId', DocumentsController.getDocumentsForBusiness);
+router.get('/deliveryPersons/:deliveryPersonId', DocumentsController.getDocumentsForDeliveryPerson);
+router.get('/drivers/:driverId', DocumentsController.getDocumentsForDriver);
+router.get('/vehicles/:vehicleId', DocumentsController.getDocumentsForVehicle);
+router.get('/type/:documentType', DocumentsController.getDocumentsByDocumentType);
+router.get('/business/:businessId/type/:documentType', DocumentsController.getDocumentsForBusinessByDocumentType);
+router.get('/drivers/:driverId/type/:documentType', DocumentsController.getDocumentsForDriverByDocumentType);
+router.get('/deliveryPersons/:deliveryPersonId/type/:documentType', DocumentsController.getDocumentsForDeliveryPersonByDocumentType);
+router.get('/vehicles/:vehicleId/type/:documentType', DocumentsController.getDocumentsForVehicleByDocumentType);
+
+router.post('/create/user', DocumentsController.createUserDocument);
+router.post('/create/business', DocumentsController.createBusinessDocument);
+router.post('/create/driver', DocumentsController.createDriverDocument);
+router.post('/create/vehicle', DocumentsController.createVehicleDocument);
+router.post('/create/deliveryPerson', DocumentsController.createDeliveryPersonDocument);
+
+router.patch('/expirationDate', DocumentsController.updateDocumentExpirationDate);
+router.patch('/issueDate', DocumentsController.updateDocumentIssueDate);
+router.patch('/files', DocumentsController.updateDocumentFiles);
+router.patch('/additionalInfo', DocumentsController.updateDocumentAdditionalInfo);
+
+module.exports = router;
