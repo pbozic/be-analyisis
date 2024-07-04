8efc568562cef940336d2c6ddc954c449087c0c3
Primož Božič <primoz.bozic@signapps.si>
2024-07-04 15:38:34 +0200

    fix review joi validation

--- joi/reviewSchemas.js
+++ joi/reviewSchemas.js
@@ -1,12 +1,12 @@
 const Joi = require("joi");
 
 const reviewBusinessSchema = Joi.object({
-    business_id: Joi.number().required(),
+    business_id: Joi.string().required(),
     comment: Joi.string().required(),
     rating: Joi.number().required(),
 });
 const reviewUserSchema = Joi.object({
-    user_id: Joi.number().required(),
+    user_id: Joi.string().required(),
     comment: Joi.string().required(),
     rating: Joi.number().required(),
 });
