98bc786326e6e893df3435442af750849ff0a5d7
Tadej Hiti <hiti.tadej@gmail.com>
2024-07-30 15:24:08 +0200

    Update TaxiOrderController.js

--- controllers/TaxiOrderController.js
+++ controllers/TaxiOrderController.js
@@ -200,6 +200,8 @@ async function createOrderHelper(req, res, orderData) {
 		let is_repeat = false;
 		console.log("body repeat", prefs)
 		if (prefs.repeat_ride && prefs.repeat_ride.some(item => item.value === "do_not_repeat")) {
+			is_repeat = false;
+		} else if (prefs.repeat_ride){
 			is_repeat = true;
 		}
 		orderData.is_scheduled = is_scheduled;
