fab684c540ac8d6360706a2166b7842915d8aa53
Primož Božič <primoz.bozic@signapps.si>
2024-08-05 16:03:15 +0200

    20sec interval for search

--- cron.js
+++ cron.js
@@ -1,11 +1,13 @@
 const cron = require("node-cron");
-const { checkIfOrdersNeedSending } = require("./lib/taxiHelpers");
+const { checkIfOrdersNeedSending, searchAfter20Seconds, searchAfter40Seconds } = require("./lib/taxiHelpers");
 const { checkIfDeliveryOrdersNeedSending, checkIfRestaurantOrderIsPrepared } = require("./lib/deliveryHelpers");
 const  { chekPingStatus } = require("./lib/driverHelpers");
 
 function startCronJobs() { 
     // Every minute
     cron.schedule("* * * * *", checkIfOrdersNeedSending);
+    cron.schedule("* * * * *", searchAfter20Seconds);
+    cron.schedule("* * * * *", searchAfter40Seconds);
     cron.schedule("* * * * *", checkIfDeliveryOrdersNeedSending);
     cron.schedule("* * * * *", checkIfRestaurantOrderIsPrepared);
     cron.schedule("* * * * *", chekPingStatus);
