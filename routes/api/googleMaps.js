b7f4d093ec4f8c767d45ae1b57a54b26a943c742
Teodora Taleska <taleska000@gmail.com>
2024-09-11 19:24:29 +0200

    implemented function to fetch address predictions from Google Maps API

--- routes/api/googleMaps.js
+++ routes/api/googleMaps.js
@@ -1,7 +1,8 @@
 var express = require("express");
-const { geocodeAddress } = require("../../lib/gApis");
+const { geocodeAddress, getPlacePredictions } = require("../../controllers/GoogleMapsController");
 const router = express.Router();
 
+router.get('/autocomplete', getPlacePredictions);
 router.post('/geocode_address', geocodeAddress);
 
 module.exports = router;
