bcc32615091ed63c88d2aaa6015df076dde3cb25
Marcel Martinšek <mm0445@student.uni-lj.si>
2024-11-13 14:50:33 +0100

    changed findUnique to findFirst

--- joi/authSchemas.js
+++ joi/authSchemas.js
@@ -18,7 +18,7 @@ const registerSchema = Joi.object({
 			console.log("helpers", helpers.error);
 			let isEmailInUse;
 			try {
-				isEmailInUse = await prisma.users.findUnique({
+				isEmailInUse = await prisma.users.findFirst({
 					where: {
 						email: email.toLowerCase(),
 					},
@@ -71,27 +71,27 @@ const registerChildSchema = Joi.object({
 	last_name: Joi.string().required(),
 	email: Joi.string()
 		.email()
-		.optional(),
-		// .external(async (email, helpers) => {
-		// 	console.log("helpers", helpers.error);
-		// 	let isEmailInUse;
-		// 	try {
-		// 		isEmailInUse = await prisma.users.findUnique({
-		// 			where: {
-		// 				email: email.toLowerCase(),
-		// 			},
-		// 		});
-		// 		console.log(isEmailInUse)
-		// 	} catch (error) {
-		// 		console.log(error)
-		// 	}
-		//
-		// 	if (isEmailInUse) {
-		// 		throw new Error("Account with this email already exists.");
-		// 	}
-		//
-		// 	return email;
-		// }),
+		.optional()
+		.external(async (email, helpers) => {
+			console.log("helpers", helpers.error);
+			let isEmailInUse;
+			try {
+				isEmailInUse = await prisma.users.findFirst({
+					where: {
+						email: email.toLowerCase(),
+					},
+				});
+				console.log(isEmailInUse)
+			} catch (error) {
+				console.log(error)
+			}
+
+			if (isEmailInUse) {
+				throw new Error("Account with this email already exists.");
+			}
+
+			return email;
+		}),
 	password: Joi.string()
 		.pattern(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$/)
 		.required(),
