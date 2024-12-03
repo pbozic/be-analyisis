897350a8eb91cb300d5053163262c54f34a3ed49
Marcel Martinšek <mm0445@student.uni-lj.si>
2024-12-03 10:48:48 +0100

    [KLIKNI-295] update requires for testing

--- localisations/texts.js
+++ localisations/texts.js
@@ -1,13 +1,13 @@
-const { en } = require("./languages/en");
-const { si } = require("./languages/si");
-const { it } = require("./languages/it");
-const { de } = require("./languages/de");
-const { es } = require("./languages/es");
-const { srb } = require("./languages/srb");
-const { bih } = require("./languages/bih");
-const { cro } = require("./languages/cro");
-const { fr } = require("./languages/fr");
-const { ru } = require("./languages/ru");
+const en  = require("./languages/en");
+const si = require("./languages/si");
+const it = require("./languages/it");
+const de = require("./languages/de");
+const es = require("./languages/es");
+const srb = require("./languages/srb");
+const bih = require("./languages/bih");
+const cro = require("./languages/cro");
+const fr = require("./languages/fr");
+const ru = require("./languages/ru");
 
 const languageTexts = {
     en,
