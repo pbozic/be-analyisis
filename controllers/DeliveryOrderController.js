232836c364c5366df66f2e5c9bd4a7dfd653f627
Marcel Martinšek <mm0445@student.uni-lj.si>
2025-02-10 14:18:16 +0100

    remove platform cut payment when completing order

--- controllers/DeliveryOrderController.js
+++ controllers/DeliveryOrderController.js
@@ -596,8 +596,6 @@ async function completeOrder(req, res) {
 			// 	}
 			// );
 		}
-		//Transfer our share of the delivery_earnings to the delivery business account
-		const transferDeliveryEarnings = stripe.transferToConnectedAccount(Math.round((order.details.delivery_earnings - order.details.delivery_cost)*100), delivery_business.stripe_account_id, order.order_id);
 
 
 		io.to("order_" + order.order_id).emit("order_completed__delivery", order);
