f88a05ddd438ea631e129e73c34442bea559fef7
Primož Božič <primoz.bozic@signapps.si>
2025-03-18 11:10:11 +0100

    logs

--- cron.js
+++ cron.js
@@ -12,7 +12,7 @@ function startCronJobs() {
     cron.schedule("* * * * *", TaxiHelpers.searchAfter40Seconds);
     cron.schedule("* * * * *", DeliveryHelpers.checkIfDeliveryOrdersNeedSending);
     cron.schedule("* * * * *", DeliveryHelpers.checkIfRestaurantOrderIsPrepared);
-    cron.schedule("* * * * *", checkPingStatus);
+    //cron.schedule("* * * * *", checkPingStatus);
     cron.schedule("* * * * *", TaxiHelpers.revokeAcceptedOrdersFromDriverHandler);
     cron.schedule("* * * * *", TaxiHelpers.scheduledOrdersNotificationsHandler);
     cron.schedule("* * * * *", TaxiHelpers.closeScheduledOrders);
