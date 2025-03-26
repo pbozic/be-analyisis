a1487297f56485acf9d3f6b6edfb752cea7464c6
Marcel Martinšek <mm0445@student.uni-lj.si>
2025-03-26 16:03:58 +0100

    migrations update

--- prisma/migrations/20250313124817_add_default_to_promo_sextions_buy_id/migration.sql
+++ prisma/migrations/20250313124817_add_default_to_promo_sextions_buy_id/migration.sql
@@ -10,9 +10,3 @@
 ALTER TABLE "promo_sections" ALTER COLUMN "t1price" SET DATA TYPE INTEGER,
 ALTER COLUMN "t2price" SET DATA TYPE INTEGER,
 ALTER COLUMN "t3price" SET DATA TYPE INTEGER;
-
--- AlterTable
-ALTER TABLE "promo_sections_buy" ADD COLUMN     "user_id" UUID;
-
--- AddForeignKey
-ALTER TABLE "promo_sections_buy" ADD CONSTRAINT "promo_sections_buy_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "users"("user_id") ON DELETE SET NULL ON UPDATE CASCADE;
