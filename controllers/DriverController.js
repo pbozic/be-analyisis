27ee8f01cedbad11b1f54dbfe810d55c78a94cf9
vreca123 <tv5870@student.uni-lj.si>
2025-04-02 16:04:07 +0200

    file handling

--- controllers/DriverController.js
+++ controllers/DriverController.js
@@ -538,6 +538,7 @@ async function createDriver(req, res) {
 					let base64 = file.base64;
 					delete file.base64;
 					delete file.name;
+					delete file.document_type;
 					let fileData = await FileDao.addFileToDocument(document.document_id, file, document.public);
 
 					let key = S3Helper.getFileKey(fileData.file_id, file.mime_type);
@@ -561,6 +562,7 @@ async function createDriver(req, res) {
 					let base64 = file.base64;
 					delete file.base64;
 					delete file.name;
+					delete file.document_type;
 					let fileData = await FileDao.addFileToDocument(document.document_id, file, document.public);
 					let key = S3Helper.getFileKey(fileData.file_id, file.mime_type);
 					S3Helper.SaveObject(key, base64, file.mime_type, {
