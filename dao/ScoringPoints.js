b63e47b6271abeecf49708e1fc79bc9ffc3538ff
Marcel Martinšek <mm0445@student.uni-lj.si>
2025-04-02 15:51:09 +0200

    [KLIKNI-764] add scoring_points model + CRUD Dao

---
+++ dao/ScoringPoints.js
@@ -0,0 +1,110 @@
+const prisma = require("../prisma/prisma");
+
+async function createScoringPoints(business_id, user_id, delivery_order_id, taxi_order_id, points, isPenalty, seconds, reason) {
+	try {
+		const newScoringPoints = await prisma.scoring_points.create({
+			data: {
+				business: { connect: { business_id: business_id } },
+				user: { connect: { user_id: user_id } },
+				delivery_order: delivery_order_id ? { connect: { order_id: delivery_order_id } } : undefined,
+				taxi_order: taxi_order_id ? { connect: { order_id: taxi_order_id } } : undefined,
+				points: points,
+				isPenalty: isPenalty,
+				seconds: seconds,
+				reason: reason
+			}
+		});
+		console.info("Created scoring points: ", newScoringPoints);
+		return newScoringPoints;
+	} catch (error) {
+		console.error('Error creating scoring points:', error);
+		throw error;
+	}
+}
+
+async function getScoringPointsById(scoring_points_id) {
+	try {
+		const scoringPoints = await prisma.scoring_points.findUnique({
+			where: {
+				scoring_points_id: scoring_points_id
+			},
+			include: {
+				users: true,
+				businesses: true,
+				delivery_orders: true,
+				taxi_orders: true
+			}
+		});
+		return scoringPoints;
+	} catch (error) {
+		console.error("Error retrieving scoring points by ID:", error);
+		throw error;
+	}
+}
+
+async function getScoringPointsByUserId(user_id) {
+	try {
+		const scoringPoints = await prisma.scoring_points.findMany({
+			where: {
+				user_id: user_id
+			},
+			orderBy: {
+				delivery_order_id: 'asc'
+			},
+			include: {
+				users: true,
+				businesses: true,
+				delivery_orders: true,
+				taxi_orders: true
+			}
+		});
+		return scoringPoints;
+	} catch (error) {
+		console.error("Error retrieving scoring points by user ID:", error);
+		throw error;
+	}
+}
+
+async function updateScoringPoints(scoring_points_id, data) {
+	try {
+		const updatedScoringPoints = await prisma.scoring_points.update({
+			where: {
+				scoring_points_id: scoring_points_id
+			},
+			data: data,
+			include: {
+				users: true,
+				businesses: true,
+				delivery_orders: true,
+				taxi_orders: true
+			}
+		});
+		console.info("Updated scoring points: ", updatedScoringPoints);
+		return updatedScoringPoints;
+	} catch (error) {
+		console.error("Error updating scoring points:", error);
+		throw error;
+	}
+}
+
+async function deleteScoringPoints(scoring_points_id) {
+	try {
+		await prisma.scoring_points.delete({
+			where: {
+				scoring_points_id: scoring_points_id
+			}
+		});
+		console.log(`Scoring points with ID ${scoring_points_id} has been deleted.`);
+	} catch (error) {
+		console.error(`Error deleting scoring points with ID ${scoring_points_id}:`, error);
+		throw error;
+	}
+}
+
+module.exports = {
+	createScoringPoints,
+	getScoringPointsById,
+	getScoringPointsByUserId,
+	updateScoringPoints,
+	deleteScoringPoints
+}
