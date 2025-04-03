2f91f0821e24bae9901bbf5615e3a4747bce6ef4
vreca123 <tv5870@student.uni-lj.si>
2025-04-03 15:26:28 +0200

    user marketing/ads/newsletter

---
+++ prisma/migrations/20250403132417_new_user_fields/migration.sql
@@ -0,0 +1,4 @@
+-- AlterTable
+ALTER TABLE "users" ADD COLUMN     "allow_ads_personalization" BOOLEAN,
+ADD COLUMN     "allow_marketing_push_notifications" BOOLEAN,
+ADD COLUMN     "allow_newsletter" BOOLEAN;
