6bb84d43cdd0a9348894088230cc9a8f3412ad0e
Primož Božič <primoz.bozic@signapps.si>
2025-04-10 13:34:44 +0200

    remove unsuported field

---
+++ prisma/migrations/20250410113418_remove_unsupported_geometry_field_to_municipalities/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `geom_generated` on the `municipalities` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "municipalities" DROP COLUMN "geom_generated";
