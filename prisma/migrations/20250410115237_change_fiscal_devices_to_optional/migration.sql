f76f46d7a9a8ee022381fbdb63cb31f4dbfa65e4
Primož Božič <primoz.bozic@signapps.si>
2025-04-10 13:58:07 +0200

    add fiscal devices

--- prisma/migrations/20250410115237_change_fiscal_devices_to_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "business" DROP CONSTRAINT "business_fiscal_devices_id_fkey";
-
--- AlterTable
-ALTER TABLE "business" ALTER COLUMN "fiscal_devices_id" DROP NOT NULL;
-
--- AddForeignKey
-ALTER TABLE "business" ADD CONSTRAINT "business_fiscal_devices_id_fkey" FOREIGN KEY ("fiscal_devices_id") REFERENCES "fiscal_devices"("fiscal_devices_id") ON DELETE SET NULL ON UPDATE CASCADE;
