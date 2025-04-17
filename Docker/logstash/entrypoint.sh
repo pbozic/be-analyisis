045d18f22ce466a3ad03a5f09806cf4102024ce4
Primož Božič <primoz.bozic@signapps.si>
2025-04-17 17:46:24 +0200

    docker

--- Docker/logstash/entrypoint.sh
+++ Docker/logstash/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-echo "📄 Generating logstash.conf from template using env substitution..."
+substitute_env() {
+  # Replace all ${VAR} occurrences with actual env values
+  perl -pe 's/\$\{(\w+)\}/$ENV{$1}/g' "$1"
+}
+echo "⏳ Waiting for /init-status/init-complete.lock..."
 
-# Shell-style substitution
-eval "echo \"$(cat /usr/share/logstash/pipeline/logstash.template.conf)\"" > /usr/share/logstash/pipeline/logstash.conf
+while [ ! -f /init-status/init-complete.lock ]; do
+  sleep 2
+done
+
+echo "✅ Init done. Starting Kibana..."
+echo "📄 Generating logstash.conf from template using env substitution..."
+substitute_env /usr/share/logstash/pipeline/logstash.template.conf > /usr/share/logstash/pipeline/logstash.conf
+substitute_env /usr/share/logstash/config/logstash.template.yml > /usr/share/logstash/config/logstash.yml
 
 echo "🚀 Starting Logstash..."
 exec logstash "$@"
