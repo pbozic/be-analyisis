01d116d338fdf61ca5b3a446d1ea3cb98d7b9d1c
Marcel Martinšek <mm0445@student.uni-lj.si>
2025-04-23 11:03:08 +0200

    update script to handle spaces in pathnames

--- Docker/dev-up.js
+++ Docker/dev-up.js
@@ -9,7 +9,7 @@ const script = isWin
   : path.join(__dirname, 'dev-up.sh');
 
 try {
-  execSync(script, { stdio: 'inherit', shell: true });
+  execSync(`\"${script}\"`, { stdio: 'inherit', shell: true });
 } catch (err) {
   console.error('❌ Failed to run dev script:', err.message);
   process.exit(1);
