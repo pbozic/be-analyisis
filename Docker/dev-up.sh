b4809068dfe563fa946e41524741f4fd23cac0c9
Primoz Bozic <primoz.bozic@signapps.si>
2025-04-23 11:14:04 +0200

    linux eol

--- Docker/dev-up.sh
+++ Docker/dev-up.sh
@@ -2,7 +2,7 @@
 
 # Load .env and extract ENVIRONMENT
 ENV=$(grep ENVIRONMENT .env | cut -d '=' -f2 | tr -d '\r')
-
+cat ENV
 if [ "$ENV" = "production" ]; then
   echo "🚀 Starting in PRODUCTION mode..."
   docker compose --env-file .env -f ./Docker/docker-compose.yml up --build -d
