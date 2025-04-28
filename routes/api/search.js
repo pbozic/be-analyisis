4e388e13c9f926a0588d87ea8f6c985035a27a0d
Primož Božič <primoz.bozic@signapps.si>
2025-04-28 11:18:16 +0200

    move promo with merchant to non auth

--- routes/api/search.js
+++ routes/api/search.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const BusinessController = require("../../controllers/BusinessController");
 
 router.get("/:business_id", BusinessController.getBusinessForSearchById);
+router.post("/sections/merchant", BusinessController.listPromoSectionsWithMerchants);
 router.post("/", BusinessController.searchBusinesses);
 router.get("/", BusinessController.getBusinessesByNameSearch);
 
