b69b49021b943e1d550e5064a23cc8e72808c72e
Primož Božič <primoz.bozic@signapps.si>
2025-05-16 14:58:25 +0200

    fix ES url

--- elasticsearch/client.js
+++ elasticsearch/client.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { Client } = require('@elastic/elasticsearch');
 console.log({
-  node: process.env.ELASTICSEARCH_HOSTS,
+  node: process.env.ELASTIC_URL,
   auth: {
     username: process.env.ELASTIC_USERNAME,
     password: process.env.ELASTIC_PASSWORD
   },
 })
 const esClient = new Client({
-    node: process.env.ELASTICSEARCH_HOSTS,
+    node: process.env.ELASTIC_URL,
     auth: {
       username: process.env.ELASTIC_USERNAME,
       password: process.env.ELASTIC_PASSWORD
