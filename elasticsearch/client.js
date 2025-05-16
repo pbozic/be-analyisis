55caf584d23af5cd83b98e6871fb5bdedbb5f65c
Tadej Hiti <hiti.tadej@gmail.com>
2025-05-16 14:44:55 +0200

    update gitignore

--- elasticsearch/client.js
+++ elasticsearch/client.js
@@ -2,14 +2,14 @@ const dotenv = require('dotenv');
 dotenv.config();
 const { Client } = require('@elastic/elasticsearch');
 console.log({
-  node: process.env.ELASTIC_URL,
+  node: process.env.ELASTICSEARCH_HOSTS,
   auth: {
     username: process.env.ELASTIC_USERNAME,
     password: process.env.ELASTIC_PASSWORD
   },
 })
 const esClient = new Client({
-    node: process.env.ELASTIC_URL,
+    node: process.env.ELASTICSEARCH_HOSTS,
     auth: {
       username: process.env.ELASTIC_USERNAME,
       password: process.env.ELASTIC_PASSWORD
