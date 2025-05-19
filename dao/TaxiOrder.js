baf1cb032940e0ebf39f5306212051129ab24b52
vreca123 <tv5870@student.uni-lj.si>
2025-05-19 14:29:42 +0200

    fetch active ids change

--- dao/TaxiOrder.js
+++ dao/TaxiOrder.js
@@ -1006,7 +1006,10 @@ async function getActiveOrderIdsForUser(user_id, scheduled = true) {
                         TAXI_ORDER_STATUS.AWAITING_PAYMENT//TODO: Should we consider AWAIITING_PAYMENT as active order in the user's eyes?
                     ]
                 },
-                is_scheduled: scheduled
+                is_scheduled: scheduled,
+                creating_user_id: {
+                    not: user_id
+                }
             },
             select:{
                 order_id:true
