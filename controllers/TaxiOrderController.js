700e0ce11db969e010588643b77a06976c1231b4
vreca123 <tv5870@student.uni-lj.si>
2025-05-20 11:31:43 +0200

    fetch active orders fix

--- controllers/TaxiOrderController.js
+++ controllers/TaxiOrderController.js
@@ -69,7 +69,7 @@ async function getActiveTaxiOrders(req, res) {
 
 	try {
 		const activeOrders = await TaxiOrderDao.getTaxiOrdersIfNotCompleted(user_id, type);
-
+		console.log(`ACTIVE ${type} ORDERS: `, activeOrders?.length);
 		if (activeOrders) {
 			// Iterate over the list of active orders
 			for (let i = 0; i < activeOrders.length; i++) {
@@ -1215,7 +1215,7 @@ async function completeOrder(req, res) {
 					}
 				}
 
-			}else{
+			} else {
 				//handle regular payments
 				//CALCULATE IN CENTS
 				const PRICE_CENTS = Math.round(parseFloat(order.payment.price) * 100)
