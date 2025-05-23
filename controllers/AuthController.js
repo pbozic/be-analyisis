e634fba868a446721aeab6282570b84746dd0daf
Primož Božič <primoz.bozic@signapps.si>
2025-05-23 14:04:50 +0200

    fix: include business_users_id typo

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -136,7 +136,6 @@ async function login(req, res) {
 				referral: { include: { referrer: { select: { first_name: true, last_name: true } } } },
 				user_roles: true,
 				business_users: {
-					business_users_id: true,
 					include: {
 						business: {
 							include: {
