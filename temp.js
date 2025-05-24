d97320d3e62f1fc7e35f29783c9ff539920a0e5b
Primož Božič <primoz.bozic@signapps.si>
2025-05-24 11:55:31 +0200

    fix: replace route calc with new v2 client

--- temp.js
+++ temp.js
@@ -1,11 +1,25 @@
-const fs = require('fs');
+const { getDistanceForRouteV2 } = require('./lib/gApis.js');
 
-const { getOrderAndPDF } = require('./lib/orderPdf');
-
-async function main() {
-	const order_id = '97ecda12-56f8-41f7-a2f3-a7a8f721b307'; // Replace with the actual order ID
-	let pdf = await getOrderAndPDF(order_id);
-	fs.writeFileSync('order.pdf', pdf);
+async function getDistanceForRoute() {
+	try {
+		const points = [
+			{ latitude: 46.073184167980266, longitude: 14.471815270613476 }, // origin
+			{ latitude: 46.0527405, longitude: 14.4720881 }, // waypoint
+			{ latitude: 46.06816374090053, longitude: 14.473042935132982 }, // destination
+		];
+		const result = await getDistanceForRouteV2(points);
+		console.log('Distance and duration:', result);
+		return result;
+	} catch (error) {
+		console.error('Error getting distance for route:', error);
+		throw error;
+	}
 }
 
-main();
+getDistanceForRoute()
+	.then((result) => {
+		console.log('Route details:', result);
+	})
+	.catch((error) => {
+		console.error('Failed to get route details:', error);
+	});
