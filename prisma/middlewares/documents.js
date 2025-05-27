cdb925306509ddfa7dfba9026a4026413d55054c
Primož Božič <primoz.bozic@signapps.si>
2025-05-27 14:27:10 +0200

    fix: file url

--- prisma/middlewares/documents.js
+++ prisma/middlewares/documents.js
@@ -10,7 +10,9 @@ const handleS3LinkForDocuments = async (model, operation, args, query, result) =
 				let files = document.files.map(async (file) => {
 					return {
 						...file,
-						url: await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public),
+						url: file.public
+							? await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public)
+							: file.url,
 					};
 				});
 				document.files = await Promise.all(files);
@@ -19,7 +21,9 @@ const handleS3LinkForDocuments = async (model, operation, args, query, result) =
 			let files = middlewareResult.files.map(async (file) => {
 				return {
 					...file,
-					url: await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public),
+					url: file.public
+						? await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public)
+						: file.url,
 				};
 			});
 
--- prisma/middlewares/file.js
+++ prisma/middlewares/file.js
@@ -3,13 +3,17 @@ const handleS3LinkForFiles = async (model, operation, args, query, result) => {
 	let middlewareResult = result;
 	if (Array.isArray(result)) {
 		for (let file of middlewareResult) {
-			file.url = await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public);
+			file.url = file.public
+				? await S3Helper.GetObject(S3Helper.getFileKey(file.file_id, file.mime_type), file.public)
+				: file.url;
 		}
 	} else {
-		middlewareResult.url = await S3Helper.GetObject(
-			S3Helper.getFileKey(middlewareResult.file_id, middlewareResult.mime_type),
-			middlewareResult.public
-		);
+		middlewareResult.url = middlewareResult.public
+			? await S3Helper.GetObject(
+					S3Helper.getFileKey(middlewareResult.file_id, middlewareResult.mime_type),
+					middlewareResult.public
+				)
+			: middlewareResult.url;
 	}
 	return middlewareResult;
 };
