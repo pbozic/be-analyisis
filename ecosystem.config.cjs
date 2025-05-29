6c1be6f40f1288715485e3f0dcd650063d7519ff
Primož Božič <primoz.bozic@signapps.si>
2025-05-29 16:06:34 +0200

    fix: ts-node

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,11 +1,12 @@
-export default {
+module.exports = {
 	apps: [
 		{
 			name: 'Klikni Dev',
-			script: 'server.js', // your entry file
+			script: 'server.ts', // or server.js if JS
 			interpreter: 'npx',
 			args: 'ts-node',
 			watch: ['.'],
+			ignore_watch: ['node_modules'],
 			env: {
 				NODE_ENV: 'development',
 			},
