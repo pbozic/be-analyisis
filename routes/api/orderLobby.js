93886c2fd49f0ca9f08c29dae8df3927694f21df
dugi888 <mirkodugi888@gmail.com>
2025-05-30 13:05:21 +0200

    [KLIKNI-933] changed editing users in lobby method to PUT

--- routes/api/orderLobby.js
+++ routes/api/orderLobby.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/:order_lobbies_id', OrderLobbyController.getOrderLobbyById);
 router.post('/create', OrderLobbyController.createLobby);
 router.post('/submit/:order_lobbies_id', OrderLobbyController.submitLobby);
-router.patch('/users/:order_lobbies_id', OrderLobbyController.setLobbyUsersWithLimits);
+router.put('/users/:order_lobbies_id', OrderLobbyController.setLobbyUsersWithLimits);
 router.patch('/items/:order_lobbies_id', OrderLobbyController.setUserOrderLobbyItems);
 router.delete('/cancel/:order_lobbies_id', OrderLobbyController.cancelLobby);
 router.delete('/delete_user/:order_lobbies_id/:user_id', OrderLobbyController.deleteUserFromLobby);
