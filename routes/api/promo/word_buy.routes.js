b8aeea93ac2618579c7d8435bcce7e41a805f091
zan mlakar <zanmlakar58@gmail.com>
2025-06-20 10:07:39 +0200

    fix: opened getWordBuysByBusiness endpoint

--- routes/api/promo/word_buy.routes.js
+++ routes/api/promo/word_buy.routes.js
@@ -4,7 +4,7 @@ import WordController from '../../../controllers/WordController.js';
 const router = express.Router();
 router.get('/', WordController.getAllWordBuys);
 // router.get('/word/:word', WordController.getAllWordBuysBySection);
-// router.get('/business/:business_id', WordController.getAllWordBuysByBusiness);
+router.get('/business/:business_id', WordController.getAllWordBuysByBusiness);
 // router.get('/tier/:tier', WordController.getAllWordBuysByTier);
 // router.get('/stripeSub/:stripe_subscription_id', WordController.getAllWordBuysByStripeSub);
 router.post('/', WordController.createWordBuy);
