37444f30600b54ea54a6704c914f181e4b707248
Primož Božič <primoz.bozic@signapps.si>
2025-07-16 15:35:21 +0200

    fix: prisma on req

--- app.js
+++ app.js
@@ -38,9 +38,9 @@ if (process.env.NODE_ENV !== 'test') {
 app.disable('etag');
 app.use(cors({ exposedHeaders: ['Content-Disposition'] }));
 
-app.use((req, res, next) => {
-	req.prisma = prisma;
-});
+// app.use((req, res, next) => {
+// 	req.prisma = prisma;
+// });
 
 app.post('/api/blog/upload/file', authMiddleware, upload.single('image'), (req, res) => {
 	console.log('File upload request received:', req.file);
