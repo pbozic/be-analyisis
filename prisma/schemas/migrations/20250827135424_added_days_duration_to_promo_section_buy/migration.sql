651e14a1724a561d5622ad862e0cfacec9b6f5d4
Primož Božič <primoz.bozic@signapps.si>
2025-08-27 16:01:39 +0200

    fix: fix duration migration

--- prisma/schemas/migrations/20250827135424_added_days_duration_to_promo_section_buy/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `duration` to the `promo_sections_buy` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "promo_sections_buy" ADD COLUMN     "duration" INTEGER NOT NULL;
---
+++ prisma/schemas/migrations/20250827140036_add_duration_to_promo_section_buy/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "promo_sections_buy" ADD COLUMN     "duration" INTEGER NOT NULL DEFAULT 30;
