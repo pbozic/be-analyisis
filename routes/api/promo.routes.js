c4204fd813b67daaea9d9ad4d253a5e47cdf61a7
vreca123 <tv5870@student.uni-lj.si>
2025-08-28 08:57:49 +0200

    fix: promo_section_bulk stripe payment intent success handler

--- routes/api/promo.routes.js
+++ routes/api/promo.routes.js
@@ -7,11 +7,12 @@ import sectionBuyRouter from './promo/section_buy.routes.js';
 import wordRouter from './promo/words.routes.js';
 import wordBuyRouter from './promo/word_buy.routes.js';
 import authMiddleware from '../../middleware/auth.js';
+
 const router = express.Router();
 router.use('/banners', bannerRouter);
 router.use('/ads', adRouter);
 router.use('/sections', sectionRouter);
-router.use('/section_buy', sectionBuyRouter);
+router.use('/section_buy', [authMiddleware], sectionBuyRouter);
 router.use('/words', wordRouter);
 router.use('/word_buy', [authMiddleware], wordBuyRouter);
 export default router;
