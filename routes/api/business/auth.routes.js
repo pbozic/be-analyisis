4b7e4b0ffe92389a11204cbe287ac6538307ce84
dugi888 <mirkodugi888@gmail.com>
2025-09-05 10:24:48 +0200

    fix: import hotfix

--- routes/api/business/auth.routes.js
+++ routes/api/business/auth.routes.js
@@ -4,7 +4,8 @@ import AuthController from '../../../controllers/AuthController.js';
 import { loginSchema, registerSchema, refreshSchema, resetPasswordSchema } from '../../../joi/authSchemas.js';
 import joi from '../../../middleware/joi.js';
 import BusinessController from '../../../controllers/BusinessController.js';
-import DailyMealController from '../../../controllers/DailyMealController.js';
+import DailyMealCategoryController from '../../../controllers/DailyMealCategoryController.js';
+
 const router = express.Router();
 router.post('/register', AuthController.registerBusiness);
 router.get('/businesses', BusinessController.listBusinesses);
