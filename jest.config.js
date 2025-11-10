eeef5ae5a9539dca4976fa672f17bcf868a0d7d2
pbozic <primoz.bozic@signapps.si>
2025-11-10 19:19:01 +0000

    feature: unify jest configs into jest.config.cjs (ts-jest, setupFiles, .js/.ts patterns)

--- jest.config.js
+++
@@ -1,8 +0,0 @@
-export const testEnvironment = 'node';
-export const setupFiles = ['<rootDir>/tests/setupEnv.js'];
-export const testMatch = ['**/tests/**/*.test.js'];
-export default {
-	testEnvironment,
-	setupFiles,
-	testMatch,
-};
